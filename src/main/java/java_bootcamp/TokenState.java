8cf41c8eaec23014d88f29d7d46d28c4fc609902
fyang1024 <fyang1024@gmail.com>
2018-08-25 11:23:26 +1000

    complete TokenState

--- src/main/java/java_bootcamp/TokenState.java
+++ src/main/java/java_bootcamp/TokenState.java
@@ -3,13 +3,43 @@ package java_bootcamp;
 import com.google.common.collect.ImmutableList;
 import net.corda.core.contracts.ContractState;
 import net.corda.core.identity.AbstractParty;
-import net.corda.core.identity.Party;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
 /* Our state, defining a shared fact on the ledger.
  * See src/main/kotlin/examples/IAmAState.java and
  * src/main/kotlin/examples/IAmAlsoAState.java for examples. */
-public class TokenState {
+public class TokenState implements ContractState {
 
+    private AbstractParty issuer, recipient;
+    private int amount;
+
+    public TokenState(AbstractParty issuer, AbstractParty recipient, int amount) {
+        this.issuer = issuer;
+        this.recipient = recipient;
+        this.amount = amount;
+    }
+
+    @NotNull
+    @Override
+    public List<AbstractParty> getParticipants() {
+        return ImmutableList.of(issuer, recipient);
+    }
+
+    public AbstractParty getOwner() {
+        return getRecipient();
+    }
+
+    public AbstractParty getIssuer() {
+        return issuer;
+    }
+
+    public AbstractParty getRecipient() {
+        return recipient;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
 }
